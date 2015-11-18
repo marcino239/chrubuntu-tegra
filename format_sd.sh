f7a6ed507fc9ef78ba38ba05312db2e724043d47
Luís de Sousa <luis.a.de.sousa@gmail.com>
2015-11-18 12:21:27 +0100

    Target disk and rootfs size now taken as inputs

--- format_sd.sh
+++ format_sd.sh
@@ -1,9 +1,15 @@
-# Brainded formatting of SD in /dev/mmcblk0
-# Assumes SD is 32 Gb and all space is to be used
-# Needs lot of expansion to become generic
+#
+# Formats an SD card with the required partitions to install Chrubuntu.
+# These partitions are:
+# . boot kernel 
+# . rootfs (where Chrubuntu can be installed)
+# This script requires the cgpt package (avaliable in Debian base systems).
+#
+# Usage example: format_sh mmblpc1 60335007
 
-# target_disk=$1
-target_disk="/dev/mmcblk0"
+target_disk=$1
+rootfs_size=$2
+
 echo "Got ${target_disk} as target drive"
 echo ""
 echo "WARNING! All data on this device will be wiped out! Continue at your own risk!"
@@ -16,6 +22,6 @@ echo "Creating kernel partition ..."
 cgpt add -i 6 -b 64 -s 32768 -S 1 -P 5 -l KERN-A -t "kernel" ${target_disk}
 
 echo "Creating rootfs partition ..."
-cgpt add -i 7 -b 32832 -s 60335007 -l ROOT-A -t "rootfs" ${target_disk}
+cgpt add -i 7 -b 32832 -s ${rootfs_size} -l ROOT-A -t "rootfs" ${target_disk}
 
 echo "Done!"
